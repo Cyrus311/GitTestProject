a072df6136f610262b7ab9d566fcee97d45849d7
Cyrus311 <cmercan@netas.lab.nortel.com>
2017-02-08 08:07:08 +0300

    test added

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace GitTestProject
             Console.WriteLine("Can GARDAŞ");
             Console.WriteLine("Mustaf GARDAŞ");
             System.Console.WriteLine("GARDAŞ");
+            System.Console.WriteLine("Test");
             Console.ReadKey();
         }
     }
